748f3fd19217191a82d0103e1546c6b351f31f90
pablo <pablonigussie1@gmail.com>
2025-06-26 21:28:51 +0300

    Enable CORS for backend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000; // You can choose any port
+
+app.use(cors()); // Enable CORS for all routes
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
